50913610b5f03e90c49680ee38eb2309b84d661f
Will Studabaker <will.studabaker@gmail.com>
2020-10-16 19:11:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = {
   prince: 'Purple Rain'
 }
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['artistName'] = 'songTitle';
+  playlist[artistName] = songTitle;
   return playlist
 }
 function removeFromPlaylist(playlist, artistName){
-  delete playlist['artistName'];
+  delete playlist[artistName];
   return playlist
 }
